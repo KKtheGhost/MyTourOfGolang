89c745fdfd520db0424c2ee1ff780e0c33d68329
fangwei <kivinsae@gmail.com>
2020-04-10 17:50:50 +0800

    1-c-17 Update

--- GoTour/1.BasicConcept/c.UsefulTypes/c-17-RangeContinued.go
+++ GoTour/1.BasicConcept/c.UsefulTypes/c-17-RangeContinued.go
@@ -1,1 +1,33 @@
+//Range continued
+//You can skip the index or value by assigning to _.
+//
+//for i, _ := range pow
+//for _, value := range pow
+//If you only want the index, you can omit the second variable.
+//
+//for i := range pow
+//这里解释一个很常用的下划线的作用，本质就是ignore一个值。在range这个功能里可以是index，也可以是value
 package main
+
+import "fmt"
+
+func GetValue(dic []int) []int {
+	for i, _ := range dic {
+		dic[i] = i << uint(i)
+	}
+	return dic
+}
+
+func GetIndex(dic []int) []int {
+	idic := make([]int, len(dic))
+	for i, _ := range dic {
+		idic[i] = i
+	}
+	return idic
+}
+
+func main() {
+	array := make([]int, 7)
+	fmt.Println(GetIndex(array))
+	fmt.Println(GetValue(array))
+}
