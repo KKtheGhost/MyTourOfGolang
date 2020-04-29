3ca7ab20e0cef14ecd78e003ac9a3bccf482f2bd
fangwei <kivinsae@gmail.com>
2020-04-29 16:52:54 +0800

    GetAliyunAddrID Metrix Export Finished

--- GetInvoiceAliyun/core/GetInvoiceAddressID.go
+++ GetInvoiceAliyun/core/GetInvoiceAddressID.go
@@ -14,7 +14,17 @@ import (
 var GetAddressErrorCode = map[int]string{
 	1: "ERROR! GAE001: Missing Csv FIle - Cannot Read the DecryptCode file or the file is not existing.",
 	2: "ERROR! GAE002: Error Building Client - Please check out network status and browser settings.",
+	3: "ERROR! GAE003: Error Invoice Request - Please contact Aliyun support to figure out causes.",
 }
+
+type AliyunAddrMetrix struct {
+	AliyunAddrProjName    string
+	AliyunAddressID       int64
+	AliyunAddressee       string
+	AliyunDeliveryAddress string
+}
+
+var EncryptedMetrixLen int = len(CsvFilter.CsvConvert())
 
 //解密单个AliyunTokenSet的密钥
 func TokenDecrypt(EncryptedMetrix CsvFilter.AliyunTokenSet) (accessKey string, accessSecret string) {
@@ -31,8 +41,6 @@ func TokenDecrypt(EncryptedMetrix CsvFilter.AliyunTokenSet) (accessKey string, a
 	return OriginAccessKey, OriginAccessSecret
 }
 
-var AliyunID int = 3
-
 //获取单个阿里云账号的Address
 func GetInvoiceAddress(AliyunID int) (response *bssopenapi.QueryCustomerAddressListResponse) {
 	//入参是AliyunID,通过ID,在AliyunTokenSet这个结构体中去找正确的key和secret.
@@ -45,13 +53,33 @@ func GetInvoiceAddress(AliyunID int) (response *bssopenapi.QueryCustomerAddressL
 	AliyunInvoiceRequest.Scheme = "https"
 	AliyunInvoiceResponse, AliyunInvoiceErr := AliyunInvoiceClient.QueryCustomerAddressList(AliyunInvoiceRequest)
 	if AliyunInvoiceErr != nil {
-		fmt.Print(AliyunInvoiceErr.Error())
+		fmt.Println(GetAddressErrorCode[3])
 	}
 	return AliyunInvoiceResponse
+	//返回的是一个合集，需要筛选生成结构体
 }
 
+func GenerateAddrMetrix() map[int]AliyunAddrMetrix {
+	AddrMetrixRes := make(map[int]AliyunAddrMetrix)
+	for AliyunID := 1; AliyunID <= EncryptedMetrixLen; AliyunID++ {
+		AddrList := GetInvoiceAddress(AliyunID).Data.CustomerInvoiceAddressList.CustomerInvoiceAddress
+		AddrNum := len(AddrList)
+		AddrProjName := CsvFilter.CsvConvert()[AliyunID].AliyunNicknam
+		for AddrID := 0; AddrID < AddrNum; AddrID++ {
+			if AddrList[AddrID].Addressee == "方苇" {
+				AddrMetrix := AliyunAddrMetrix{AddrProjName, AddrList[AddrID].Id, AddrList[AddrID].Addressee, AddrList[AddrID].DeliveryAddress}
+				AddrMetrixRes[AliyunID] = AddrMetrix
+			} else {
+				continue
+			}
+		}
+	}
+	return AddrMetrixRes
+}
+
+//打包TokenDecrypt和GetInvoiceAddress到循环中，输出一个有效的的结构体，包含ID，UserName，项目名。
+
 func main() {
-	fmt.Printf("%#v\n", GetInvoiceAddress(AliyunID).Data.CustomerInvoiceAddressList.CustomerInvoiceAddress[3].Id) //.Data.CustomerInvoiceAddressList.CustomerInvoiceAddress)
-	fmt.Printf("%#v\n", GetInvoiceAddress(AliyunID).Data.CustomerInvoiceAddressList.CustomerInvoiceAddress[3].Addressee)
-	fmt.Printf("%#v\n", GetInvoiceAddress(AliyunID).Data.CustomerInvoiceAddressList.CustomerInvoiceAddress[3].DeliveryAddress)
+	//fmt.Printf("%#v\n", GenerateAddrMetrix())
+	fmt.Println(GenerateAddrMetrix())
 }
